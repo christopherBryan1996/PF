820c8dffef7b7a399817be502c8cf8708437bd8b
Estanip <estani.pettigrew@gmail.com>
2021-10-11 17:21:44 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ const morgan=require('morgan')
 const cors= require('cors')
 const router = require('./src/routers/router')
 
+const pepito = "pepito"
+
 //middleware
 app.use(morgan('dev'))
 app.use(cors({
