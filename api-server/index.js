2e7a61548cf463abecc5a5a9134571fe54721cdb
cristian1017 <cristianmauriciogutierrez1998@gmail.com>
2021-10-14 15:45:30 -0500

    Autenticacion login y registro

--- api-server/index.js
+++ api-server/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan =require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -22,6 +23,10 @@ app.use(express.urlencoded({extended:false})); // reconoce objetos de solicitud 
 
 //aqui manejaremos todas las rutas
 app.use(router);
+
+
+
+
 
 app.listen(3008,()=>{
     console.log('I listen in the port: http://localhost:3008')
