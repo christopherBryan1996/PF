7a59a800cf99a43ea15536de41f0f743dc65e20b
Janku98 <facundojankunas@hotmail.com.ar>
2021-10-14 15:53:45 -0300

    links en el foot y agregar linea separadora

--- client-web/src/components/Foot.tsx
+++ client-web/src/components/Foot.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import  "./styles/Foot.css";
+import { Link } from "react-router-dom";
 
 
 
@@ -9,20 +10,21 @@ export default function Foot() {
     
     return(
         <div>
+            <div className="linea"></div>
             <div className="contenedorFinal">
                 <div className="p">
                     <p>2021 ©ClanFest All rights reserved</p>
                 </div>
                 <div className="divUl"> 
                     <ul className="listafinal">
-                        <li><p>Home</p></li>
-                        <li><p>About Us</p></li>
+                        <li> <Link to="/home"> <p>Home</p> </Link> </li>
+                        <li> <Link to="/About"> <p>About Us</p> </Link> </li>
                         <li><p>Twitter</p></li>
-                        <li><p>Facebook</p></li>
-                        <li><p>Henry</p></li>
-                        <li><p>Linkedin</p></li>
-                        <li><p>Privacy Policy</p></li>
-                        <li><p>Register</p></li>
+                        <li> <a href="https://www.facebook.com/brian.ramos.71271/videos/826579387874133/"> <p>Facebook</p> </a> </li>
+                        <li> <a href="https://www.soyhenry.com/"> <p>Henry</p> </a> </li>
+                        <li> <a href="https://www.instagram.com/reel/CTNbinrnCrI/?utm_medium=copy_link"> <p>Linkedin</p> </a> </li>
+                        <li> <a href="https://www.instagram.com/reel/CT0jN4OgC_g/?utm_medium=copy_link"> <p>Privacy Policy</p> </a> </li>
+                        <li> <Link to="/Register"> <p>Register</p> </Link> </li>
                     </ul>
                 </div>
             </div>
