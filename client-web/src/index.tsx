964cdf3209592dc61439665d317ca8453065f9d4
cristian1017 <cristianmauriciogutierrez1998@gmail.com>
2021-10-14 18:38:58 -0500

    Organizacion de estructura fron con redux

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -2,38 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route} from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import EventDetails from './components/EventDetails';
-import About from './components/About';
-import Login from './components/Login';
-import Register from './components/Register';
-import NewEvent from './components/NewEvent';
-import { Home } from './components/Home';
-import { Nav } from './components/Nav';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Route  path="/" component={Nav}/>
-      <Route exact path="/" component={LandingPage}/>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path="/app" component={App}/>
-      <Route exact path="/details" component={EventDetails} />
-      <Route exact path="/about" component={About}/>
-      <Route exact path="/Login" component={Login}/>
-      <Route exact path="/Register" component={Register}/>
-      <Route exact path="/NewEvent" component={NewEvent}/>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
-//eeerererer
---
+++ client-web/src/routers/AppRouter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import About from '../components/About';
+import EventDetails from '../components/EventDetails';
+import { Home } from '../components/Home';
+import LandingPage from '../components/LandingPage';
+import Login from '../components/Login';
+import { Nav } from '../components/Nav';
+import NewEvent from '../components/NewEvent';
+import Register from '../components/Register';
+
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                    <Route path="/" component={Nav} />
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/details" component={EventDetails} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/Login" component={Login} />
+                    <Route exact path="/Register" component={Register} />
+                    <Route exact path="/NewEvent" component={NewEvent} />
+            </div>
+        </Router>
+    )
+}
