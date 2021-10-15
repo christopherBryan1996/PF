1a3aaca6ffb406e7f708453c951ad1ed0df81d89
cristian1017 <cristianmauriciogutierrez1998@gmail.com>
2021-10-14 19:42:11 -0500

    resolver problemas y añadir mas types

--- client-web/src/types/types.ts
+++ client-web/src/types/types.ts
@@ -1,3 +1,11 @@
 export const types = {
-    authLogin: '[auth] login',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start Token Renew',
+    authLogout: '[auth] Logout',
+
 }
