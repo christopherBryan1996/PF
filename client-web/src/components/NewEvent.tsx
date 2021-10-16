5b17c53139809a912ddbd5f134ef3d21f00d57c1
Nestor Raul Camacho <nestrra@gmail.com>
2021-10-16 14:32:55 -0500

    css aComponente registro

--- client-web/src/components/NewEvent.tsx
+++ client-web/src/components/NewEvent.tsx
@@ -1,7 +1,8 @@
 import { ImArrowLeft } from "react-icons/im";
 import { useHistory } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 import './styles/NewEvent.css';
+import { FaCalendarAlt } from "react-icons/fa";
 
 export default function NewEvent() {
 
@@ -16,16 +17,16 @@ export default function NewEvent() {
     const [descripcion, setDescripcion] = useState("");
 
     //Funcion para enviar el post del form----------------------------------------------------------------
-    const handleSubmit = (e:any) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault();
-        if( !name || !ubicacion || !publicoOPriv || !numeroPersonas || !precio || !fecha || !descripcion  ){return alert("Faltan completar casillas!")}
-        
+        if (!name || !ubicacion || !publicoOPriv || !numeroPersonas || !precio || !fecha || !descripcion) { return alert("Faltan completar casillas!") }
 
-        const post = { name, ubicacion, publicoOPriv, numeroPersonas, precio, fecha, descripcion}
-        console.log("constPost",post)
+
+        const post = { name, ubicacion, publicoOPriv, numeroPersonas, precio, fecha, descripcion }
+        console.log("constPost", post)
         fetch('http://localhost:3001/activity', {
             method: 'POST',
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(post)
         })
     };
@@ -34,93 +35,160 @@ export default function NewEvent() {
     //Funcion para redirigir atras---------------------------------------------------------------------
     const history = useHistory();
     const toBack = () => {
-        history.goBack() 
+        history.goBack()
     };
 
     //Return del componente----------------------------------------------------------------------------
 
-    return(
-        <div>
-            <div className="navCrearEvento">
-                <a onClick={toBack} > <ImArrowLeft color="white" fontSize="1.8em"/></a>
-                <h1>Crear Evento</h1>
-                <button>Aca estaria la fotito de perfil</button>
-            </div>
-            <div>
-                <form onSubmit={handleSubmit} className="form">
-                    <div className="divDeLaUl">
-                    <ul className="Lista">
-                        <li>
-                            <label>Nombre del evento</label>
-                            <input
-                            placeholder="Nombre del evento"
-                            type="text"
-                            value={name}
-                            onChange={(e)=>setName(e.target.value)}></input>
-                        </li>
+    return (
 
-                        <li>
-                            <label>Ubicacion</label>
-                            <input
-                            placeholder="Ubicacion del evento"
-                            type="text"
-                            value={ubicacion}
-                            onChange={(e)=>setUbicacion(e.target.value)}
-                            ></input>
-                        </li>
+        <div className="container container-h">
+            <div className="card shadow-lg p-4 mb-5 bg-dark rounded">
+                <form className="bg-dark" />
 
-                        <li>
-                            <label>Numero de Personas</label>
-                            <input
-                            type="number"
-                            value={numeroPersonas}
-                            onChange={(e)=>setNumeroPersonas(parseInt(e.target.value))}
-                            ></input>
-                        </li>
-
-                        <li>
-                            <label>Publico o Privado</label>
-                            <select
-                            value={publicoOPriv}
-                            onChange={(e)=>setPublicoOPriv(e.target.value)}
-                            >
-                             <option value="publico">Publico</option>
-                             <option value="privado">Privado</option>
-                            </select>
-                        </li>
-
-                        <li>
-                            <label>Precio de entrada</label>
-                            <input
-                            type="number"
-                            value={precio}
-                            onChange={(e)=>setPrecio(parseInt(e.target.value))}
-                            ></input>
-                        </li>
-
-                        <li>
-                            <label>Nombre del evento</label>
-                            <input
-                            type="date"
-                            value={fecha}
-                            onChange={(e)=>setFecha(e.target.value)}
-                            ></input>
-                        </li>
-
-                    </ul>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label >Nombre de evento</label>
+                        <input type="text" className="form-control" id="inputEmail4" placeholder="Nombre evento.." />
                     </div>
-                    <div className="inputTextarea">
-                        <label>Description</label>
-                        <textarea
-                        placeholder="Descripcion del evento, detalles, cuidades, etc"
-                        value={descripcion}
-                        onChange={(e)=>setDescripcion(e.target.value)}
-                        ></textarea>
+                    <div className="form-group col-md-6">
+                        <label >Fecha</label>
+                        <input type="password" className="form-control" id="inputPassword4" placeholder="Fecha" />
                     </div>
-                    
-                    <button>Crear</button>
-                </form>
+                </div>
+                <div className="form-group">
+                    <label >Direccion</label>
+                    <input type="text" className="form-control" id="inputAddress" placeholder="Direccion" />
+                </div>
+                <div className="form-group">
+                    <label >Address 2</label>
+                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label >City</label>
+                        <input type="text" className="form-control" id="inputCity" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label >Privacidad</label>
+                        <select id="inputState" className="form-control">
+                            <option selected>Publico</option>
+                            <option>Privado</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label >Numero de invitados</label>
+                        <input type="text" className="form-control" id="inputZip" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label >Privacidad</label>
+                        <select id="inputState" className="form-control">
+                            <option selected>Publico</option>
+                            <option>Privado</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck" />
+                        <label className="form-check-label" >
+                            Check me out
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Sign in</button>
+
             </div>
         </div>
+
+
+
+
     )
 };
+
+
+
+
+
+// <div>
+// <div className="navCrearEvento">
+//     <a onClick={toBack} > <ImArrowLeft color="white" fontSize="1.8em"/></a>
+//     <h1>Crear Evento</h1>
+//     <button>Aca estaria la fotito de perfil</button>
+// </div>
+// <div>
+//     <form onSubmit={handleSubmit} className="form">
+//         <div className="divDeLaUl">
+//         <ul className="Lista">
+//             <li>
+//                 <label>Nombre del evento</label>
+//                 <input
+//                 placeholder="Nombre del evento"
+//                 type="text"
+//                 value={name}
+//                 onChange={(e)=>setName(e.target.value)}></input>
+//             </li>
+
+//             <li>
+//                 <label>Ubicacion</label>
+//                 <input
+//                 placeholder="Ubicacion del evento"
+//                 type="text"
+//                 value={ubicacion}
+//                 onChange={(e)=>setUbicacion(e.target.value)}
+//                 ></input>
+//             </li>
+
+//             <li>
+//                 <label>Numero de Personas</label>
+//                 <input
+//                 type="number"
+//                 value={numeroPersonas}
+//                 onChange={(e)=>setNumeroPersonas(parseInt(e.target.value))}
+//                 ></input>
+//             </li>
+
+//             <li>
+//                 <label>Publico o Privado</label>
+//                 <select
+//                 value={publicoOPriv}
+//                 onChange={(e)=>setPublicoOPriv(e.target.value)}                            >
+//                  <option value="publico">Publico</option>
+//                  <option value="privado">Privado</option>
+//                 </select>
+//             </li>
+
+//             <li>
+//                 <label>Precio de entrada</label>
+//                 <input
+//                 type="number"
+//                 value={precio}
+//                 onChange={(e)=>setPrecio(parseInt(e.target.value))}
+//                 ></input>
+//             </li>
+
+//             <li>
+//                 <label>Nombre del evento</label>
+//                 <input
+//                 type="date"
+//                 value={fecha}
+//                 onChange={(e)=>setFecha(e.target.value)}
+//                 ></input>
+//             </li>
+
+//         </ul>
+//         </div>
+//         <div className="inputTextarea">
+//             <label>Description</label>
+//             <textarea
+//             placeholder="Descripcion del evento, detalles, cuidades, etc"
+//             value={descripcion}
+//             onChange={(e)=>setDescripcion(e.target.value)}
+//             ></textarea>
+//         </div>
+        
+//         <button>Crear</button>
+//     </form>
+// </div>
+// </div>
--- client-web/src/components/Register.tsx
+++ client-web/src/components/Register.tsx
@@ -1,8 +1,9 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './styles/Register.css';
 import { useHistory } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FcGoogle } from "react-icons/fc";
 
 export default function Register() {
 
@@ -13,14 +14,13 @@ export default function Register() {
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
 
+    console.log(username, email, password2, password)
     //Funcion para redirigir la pagina-------------------------------------------------------
     const history = useHistory();
     const ToLogin = () => {
         history.push("/Login")
     };
-    const back = () => {
-        history.goBack() 
-    };
+    
     //warnings----------------------------------------------------------------------
     const contraseñaIncorrecta = () => toast.error('La contraseña debe tener mas de 6 caracteres y contener 1 numero!', {
         position: "top-center",
@@ -30,7 +30,7 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        });
+    });
     const mailIncorrecto = () => toast.error('Debe ingresar un mail valido', {
         position: "top-center",
         autoClose: 5000,
@@ -39,7 +39,7 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        }); 
+    });
     const contraseña2incorrecta = () => toast.error('Las contraseñas no coinciden', {
         position: "top-center",
         autoClose: 5000,
@@ -48,83 +48,108 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        });         
+    });
 
     //Funcion para enviar los posts del form-------------------------------------------------
-    const handleSubmit = (e:any) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault();
         //validators------------------------------
-        if(!username || !email || !password || !password2){return alert("Faltan completar casillas!")}
-        if (password2 !== password){return contraseña2incorrecta()}
-        const ck_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  ;
-        const ck_password =/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/ ;
-        if (!ck_password.test(password)){
+        if (!username || !email || !password || !password2) { return alert("Faltan completar casillas!") }
+        if (password2 !== password) { return contraseña2incorrecta() }
+        const ck_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const ck_password = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        if (!ck_password.test(password)) {
             return contraseñaIncorrecta()
         };
-        if(!ck_email.test(email)){
+        if (!ck_email.test(email)) {
             return mailIncorrecto()
         };
 
-        const post = {username, email, password}
-        console.log("constPost",post)
+        const post = { username, email, password }
+        console.log("constPost", post)
         fetch('http://localhost:3001/activity', {
             method: 'POST',
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(post)
         })
     };
 
-
     //Return del componente--------------------------------------------------------------------
-    return(
-        <div className="fondoDiv"> 
-            <div>
-                <button onClick={back}>Back</button>
-                <h2>Completa las casillas con tu informacion</h2>
-            </div>
-            <div>
-                <form className="formRegister" onSubmit={handleSubmit}>
-                    <label>Nombre de usuario</label>
-                    <input 
-                    placeholder="Escribe tu Nombre de usuario"
-                    type="text"
-                    value={username}
-                    onChange={(e)=> setUsername(e.target.value)}></input>
-                    <label>Email</label>
-                    <input 
-                    placeholder="Escribe tu Email"
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}></input>
-                    <label>Contraseña</label>
-                    <input 
-                    placeholder="Escribe tu contraseña"
-                    type="password"
-                    value={password}
-                    onChange={(e)=> setPassword(e.target.value)} ></input>
-                    <label> Repite tu contraseña</label>
-                    <input 
-                    placeholder="Repite tu contraseña" 
-                    type="password" 
-                    value={password2}
-                    onChange={(e)=>setPassword2(e.target.value)} ></input>
-                    <button>Register</button>
-                </form>
-            </div>
-            <div className="botonesRegister">
-                <button onClick={ToLogin}>Ya tienes cuenta? LogIn</button>
-                <h3>Or</h3>
-                <button>Inicia con Google</button>
+    return (
+        <div className="fondoDiv">
+
+            <div className="container card p-3 card-register ">
+                <div>
+
+                    <h2>Registro</h2>
+                </div>
+                <div>
+                    <form className="formRegister form-gorup" onSubmit={handleSubmit}>
+                        <div className="form-group col-md-6 ">
+                            <label>Nombre de usuario</label>
+                            <input
+                                className="form-control"
+                                placeholder="Escribe tu Nombre de usuario"
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}>
+                            </input>
+                        </div>
+                        <div className="form-group col-md-6 ">
+                            <label>Email</label>
+                            <input
+                                className="form-control"
+                                placeholder="Escribe tu Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}>
+                            </input>
+                        </div>
+                        <div className="form-group col-md-6 ">
+                            <label>Contraseña</label>
+                            <input
+                                className="form-control"
+                                placeholder="Escribe tu contraseña"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} >
+                            </input>
+                        </div>
+                        <div className="form-group col-md-6 ">
+                            <label> Repite tu contraseña</label>
+                            <input
+                                className="form-control"
+                                placeholder="Repite tu contraseña"
+                                type="password"
+                                value={password2}
+                                onChange={(e) => setPassword2(e.target.value)} >
+                            </input>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <button className="btn btn-primary col-md-12  btn-lg">Crear cuenta</button>
+                        </div>
+
+                        <div className="form-group col-md-6">
+
+                            <button className=" col-md-12  btn-lg btn btn-outline-light"> <FcGoogle fontSize="1.8em" /> Con Google</button>
+
+                        </div>
+                        <div className="form-group col-md-6">
+                            <button className="col-md-12 btn btn-success" onClick={ToLogin}>Ya tienes cuenta? LogIn</button>
+                        </div>
+                    </form>
+                </div>
+
             </div>
             <ToastContainer
-            position="top-right"
-            autoClose={1000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover/>
+                position="top-right"
+                autoClose={1000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover />
         </div>
     )
 }
