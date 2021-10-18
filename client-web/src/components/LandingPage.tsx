2377408483c3f23487f4211a4e53e43e428dd650
Nestor Raul Camacho <nestrra@gmail.com>
2021-10-18 11:59:25 -0500

    cambios estilo landing

--- client-web/src/components/LandingPage.tsx
+++ client-web/src/components/LandingPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import  "./styles/LandingPage.css";
 import imagen1 from '../images/fiestalogin.jpg';
 import imagen2 from '../images/fiestalogin2.jpg';
+import logo from '../images/Logo.png';
 import imagen3 from '../images/fiestalogin4.jpg';
 import { useHistory } from "react-router-dom";
 import Foot from './Foot';
@@ -27,55 +28,56 @@ export default function LandingPage ()  {
     //Return del componente------------------------------------------------------------------------------
     return(
         <div>
-            <div className="nav-contenedor">
+        <div className=" container nav-contenedor">
 
-                <div className="div-logo">
-                    <h1>ClanFest</h1>
-                    <button className="botonSobreNosotros" onClick={botonSobreNosotros}>Sobre Nosotros</button>
-                </div>
-                <div className="botoneslogin">
-                    <button className="botonlogin" onClick={botonIngresa}>Ingresa</button>
-                    <button onClick={botonRegistrate}>Registrate</button>
-                </div>
+            <div className="div-logo">
+
+                <a className="navbar-brand" href="#"><img src={logo} alt="" width="240" height="90" /></a>
+                <button className="btn btn-outline-success" onClick={botonSobreNosotros}>Sobre Nosotros</button>
             </div>
-
-            <div className="div-tarjeta1">
-                <div className="contTextoBoton">
-                    <h2 className="titulotarjeta2">La mejor manera de organizar y compartir tus eventos Online</h2>
-                    <p className="parrafo1">Ten el control tanto en tu computadora como en tu celular</p>
-                    <button onClick={toHomeInvitado} >Pruebalo gratis</button>
-                </div>
-                <div>
-                    <img src={imagen1} alt="imagen de vos cuando bajes la app" className="imagenes" />
-                </div>
+            <div className="botoneslogin">
+                <button className="btn btn-outline-success" onClick={botonIngresa}>Ingresa</button>
+                <button className="btn btn-outline-success" onClick={botonRegistrate}>Registrate</button>
             </div>
-
-            <div className="div-tarjeta1">
-                <div>
-                    <img src={imagen2} alt="imagen de vos cuando bajes la app" className="imagenes"/>
-                </div>
-                <div className="contTextoBoton">
-                    <h2 className="titulotarjeta2" >Agrega eventos a tu lista de Favoritos</h2>
-                    <p className="parrafo1">Arma tu bandeja de favoritos, asi no tienes tiempo que perder pensando cual es tu mejor opcion</p>
-                    <button onClick={toHomeInvitado} >Hecha un vistazo</button>
-                </div>
-            </div>
-
-            <div className="div-tarjeta1">
-                <div className="contTextoBoton">
-                    <h2 className="titulotarjeta2">Organiza eventos y compartelos en tus redes</h2>
-                    <p className="parrafo1">No te preocupes por las invitaciones, con nosotros todo es mas facil y rapido</p>
-                    <button onClick={toHomeInvitado} >Intentalo ya</button>
-                </div>
-                <div>
-                    <img src={imagen3} alt="imagen de vos cuando bajes la app" className="imagenes" />
-                </div>
-            </div>
-
-            
-
-            <Foot/>
-
         </div>
+
+        <div className="div-tarjeta1">
+            <div className="contTextoBoton">
+                <h2 className="titulotarjeta2 text-center">La mejor manera de organizar y compartir tus eventos Online</h2>
+                <p className="parrafo1">Ten el control tanto en tu computadora como en tu celular</p>
+                <button className="btn btn-success" onClick={toHomeInvitado} >Hecha un vistazo</button>
+            </div>
+            <div>
+                <img src={imagen1} alt="imagen de vos cuando bajes la app" className="imagenes" />
+            </div>
+        </div>
+
+        <div className="div-tarjeta1">
+            <div>
+                <img src={imagen2} alt="imagen de vos cuando bajes la app" className="imagenes" />
+            </div>
+            <div className="contTextoBoton">
+                <h2 className="titulotarjeta2 text-center" >Agrega eventos a tu lista de Favoritos</h2>
+                <p className="parrafo1 text-center">Arma tu bandeja de favoritos, asi no tienes tiempo que perder pensando cual es tu mejor opcion</p>
+
+            </div>
+        </div>
+
+        <div className="div-tarjeta1">
+            <div className="contTextoBoton text-center">
+                <h2 className="titulotarjeta2 text-center">Organiza eventos y compartelos en tus redes</h2>
+                <p className="parrafo1 text-center">No te preocupes por las invitaciones, con nosotros todo es mas facil y rapido</p>
+
+            </div>
+            <div className="imagen" >
+                <img src={imagen3} alt="imagen de vos cuando bajes la app" className="imagenes" />
+            </div>
+        </div>
+
+
+
+        <Foot />
+
+    </div>
     )
 }
--- client-web/src/components/Nav.tsx
+++ client-web/src/components/Nav.tsx
@@ -1,10 +1,13 @@
 import "./styles/Nav.css";
+import { useState } from "react";
 import { ImHeart } from "react-icons/im";
 import logo from '../images/Logo.png';
 import avatar from '../images/user.png'
 import { useHistory } from "react-router-dom";
 
 export const Nav = () => {
+
+    const [login, loginstate] = useState(false)
 
     const history = useHistory();
     const botonIngresa = () => {
@@ -17,18 +20,30 @@ export const Nav = () => {
 
     return (
 
+
         <nav className="navbar navbar-dark bg-dark fixed-top">
             <div className="container">
-                <a className="navbar-brand" href="#"><img src={logo} alt="" width="180" height="60" /></a>
+                <a className="navbar-brand" href="#"><img src={logo} alt="" width="240" height="90" /></a>
                 <form className="navbar-center">
                     <input type="text" className="form-control" placeholder="Buscar evento..." aria-label="Username" aria-describedby="basic-addon1" />
                 </form>
-                    <button onClick={botonIngresa} className="btn btn-outline-success">Ingresar </button>
-                    <button onClick={botonRegistrate} className="btn btn-outline-success">Registarme </button>
-                <div className="fff">
-                <a href=""> <ImHeart color="white" fontSize="1.8em"/></a>
-                <a href=""><img src={avatar} alt="" width="50" height="50" /></a>
-                </div>
+                <button onClick={botonIngresa} className="btn btn-outline-success">Ingresar </button>
+                <button onClick={botonRegistrate} className="btn btn-outline-success">Registarme </button>
+                {login ?
+                    (
+                        <div className="fff">
+                            <a href=""> <ImHeart color="white" fontSize="1.8em" /></a>
+                            <a href=""><img src={avatar} alt="" width="50" height="50" /></a>
+                        </div>
+                    ) :
+                    (
+                        <div >
+
+                        </div>
+                    )
+
+                }
+
             </div>
         </nav>
 
