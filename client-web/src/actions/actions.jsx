5e031801d392754b5ea2a79a687d0af417d24ef2
Rafael Viteri <reviterif@gmail.com>
2021-10-18 23:50:05 -0500

    eventos reales

--- client-web/src/actions/actions.jsx
+++
@@ -1,6 +0,0 @@
-export function llenarCoordenadas (data) {
-    console.log("llego action llenarCoordenadas")
-    return async function (dispatch){
-        return dispatch({type:"llenarCoordenadas", payload: data})
-    }
-}
--- client-web/src/components/Evento.tsx
+++ client-web/src/components/Evento.tsx
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 import imag from '../images/bolos.jpg';
 
 
+interface  Iprops {fecha:string,imagen:string,nombreDelEvento:string}
 
 
+export const Evento = (props:Iprops) => {
+    const {fecha,imagen,nombreDelEvento}:Iprops =props
+    
 
-export const Evento = () => {
     return (
         <>
             
             <div className="container container-card">
                 <div className="card ">
                 <Link to="/details"className="link" >
-                    <img className="card-img-top" src={imag} alt="Card image cap"  height="240" />
+                    <img className="card-img-top" src={imagen} alt="Card image cap"  height="240" />
                     <div className="card-body">
-                        <p className="card-text">20/10/2021</p>
-                        <h5 className="card-title">Jugar Bolos</h5>
+                        <p className="card-text">{fecha.slice(0,10)}</p>
+                        <h5 className="card-title">{nombreDelEvento}</h5>
                     </div>
                     </Link>
                     <div className="card-footer">
