8164a3f2369ffeec9ac46a8188d9ee2d7478d52b
NaniBM <bazanmurua@gmail.com>
2021-10-19 11:24:14 -0300

    ruta de asistentes

---
+++ client-web/src/components/Asistente.tsx
@@ -0,0 +1,8 @@
+
+export default function Asistente() {
+    return(
+        <div>
+            Asistente
+        </div>
+    )
+}
---
+++ client-web/src/components/AsistentesPage.tsx
@@ -0,0 +1,10 @@
+import  Asistente from './Asistente';
+
+
+export default function AsistentesPage() {
+    return(
+        <div>
+          <Asistente/>           
+        </div>
+    )
+}
