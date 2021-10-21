5b6bcf5a6c7125fa6379f05ef337e0a1a7e36098
Janku98 <facundojankunas@hotmail.com.ar>
2021-10-21 18:40:39 -0300

    ahora el token de login dura para siempre ahre

--- client-web/src/store/store.js
+++ client-web/src/store/store.js
@@ -2,14 +2,27 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import  {rootReducer}  from '../reducers/rootReducer';
 import {reducerCoords} from '../reducers/coordsReducer';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' 
 
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose ;
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['authGoo']
+  }
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeEnhancers(
         applyMiddleware(thunk)
     ),
 
 )
+
+
+export const persistedStore = persistStore(store)
