0bab4f746c484773bd72f6f204c59bf342dc7c49
Rafael Viteri <reviterif@gmail.com>
2021-11-01 01:47:57 -0500

    cambios

---
+++ src/categorias/Categorias.tsx
@@ -0,0 +1,11 @@
+const categorias = [
+    "deportes, salud, estilo de vida" ,
+"fiestas, conciertos, comida y bebida ",
+"educacion, negocios y tecnologia"  ,
+"online",
+"viajes, ocio y hobbies"  ,
+"hogar y familia",
+"otros" 
+]
+
+export default categorias;
