75c14d13feb6e3c3fc3013283433716aa10ea47c
Nestor Raul Camacho <nestrra@gmail.com>
2021-11-03 13:14:23 -0500

    css a favoritos

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -10,6 +10,9 @@ import { toast, ToastContainer } from 'react-toastify';
 import axios from "axios";
 import URLrequests from "./constanteURL";
 import { combinarFavs } from "../controllers/favoritosInvit/favoritosInvit";
+import { FacebookIcon, FacebookShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
+import { IoCopyOutline } from "react-icons/io5";
+import Foot from "./Foot";
 
 
 export default function Favorites() {
@@ -80,15 +83,95 @@ export default function Favorites() {
         eventosFavoritos && dispatch(deleteFavoriteInvit(eventid));
         eventoQuitado();
     }
+    const seCopio = () => toast.success('El URL del evento se copio en tu teclado', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
 
-
+    const toEventClipboard = (_id: any) => {
+       
+        var UrlCompartir = `http://localhost:3000/detail/${_id}` ;
+        navigator.clipboard.writeText(UrlCompartir);
+         seCopio();
+    }
 
 
     return (
         <div>
 
             <Nav />
-            <div className="card">
+
+            <div className="container container-favorites">
+                <div>
+                    <h2>Favoritos</h2>
+                </div>
+
+                {eventosFavoritos.favouritesEvents ? eventosFavoritos.favouritesEvents.map((e: any) => (
+
+                    <><div className="card mb-3 container-card-favorites">
+                        <div className="row no-gutters">
+
+                            <div className="col-md-4">
+                                <Link to={`/detail/${e._id}`}>
+                                    <img src={e.imagen} className="card-img" height="180px" alt="..." />
+                                </Link>
+
+                            </div>
+
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">{e.nombreDelEvento}</h5>
+                                    <p className="card-text fecha">{e.fecha.split("").slice(0, 10).join("")}</p>
+                                    <p className="card-text"><small className="text-muted">{e.precio === 0 ? 'Gratis' : `Valor: $${e.precio}`}</small></p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <span className="spa">Compartir con tus amigos</span>
+                            <FacebookShareButton url={`https://flamboyant-golick-d7cb40.netlify.app/detail/${e._id}`} quote='Hola, quiero compartir este evento'>
+                                <FacebookIcon className="share" round={true} size='2em' />
+                            </FacebookShareButton>
+                            <WhatsappShareButton
+                                title='Hola, te comparto este evento, te pueda interesar!'
+                                url={`https://flamboyant-golick-d7cb40.netlify.app/detail/${e._id}`}>
+                                <WhatsappIcon className="share" round={true} size='2em' />
+                            </WhatsappShareButton>
+                            <button
+                                className="botonCopy"
+                                onClick={() => toEventClipboard(e._id)}>
+                                <IoCopyOutline></IoCopyOutline>
+                            </button>
+                            <div className="icon">
+                                <ImBin className="icon-delete" fontSize="1.6em" onClick={() => deleteFavoriteEvent(authGoo.logNormal.uid, e._id)} />
+                            </div>
+                        </div>
+                    </div><ToastContainer
+                            position="top-right"
+                            autoClose={1000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover /></>
+
+                )) : null
+                }
+
+
+
+            </div>
+
+
+
+            {/* <div className="card">
             </div>
             <div className="DivDeArriba">
                 <div className="DivTituloFiltros">
@@ -130,8 +213,10 @@ export default function Favorites() {
 
                 )) : null
                 }
-            </div>
-
+            </div> */}
+            <Foot />
         </div>
     )
 }
+
+
