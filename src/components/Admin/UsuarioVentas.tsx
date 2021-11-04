9aac0b8f2fa957f28ba5f1b61cf920eee6d91bfb
Nestor Raul Camacho <nestrra@gmail.com>
2021-11-04 08:49:40 -0500

    trabjoAdmin

--- src/components/Admin/UsuarioVentas.tsx
+++ src/components/Admin/UsuarioVentas.tsx
@@ -1,6 +1,7 @@
 
 
 import React from 'react'
+import { useHistory } from 'react-router';
 
 
 interface Iprops {
@@ -15,19 +16,28 @@ interface Iprops {
 
 export const UsuarioVentas = (props: Iprops) => {
 
-
+    const history = useHistory();
+    const toEvent = (eventid: string) => {
+        history.push(`/detail/${eventid}`)
+    };
 
     return (
         <>
             {props.eventosaAsistir.map((e: any) => (
-                <tr><th>{props.usuario}</th>                    
-                    <th>{e.statusPago.status}</th>
-                    <th>{e.eventId?.nombreDelEvento }</th>
+                
+                <tr>
                     
+                    {(e.eventId ?
+                        <><th>{props.usuario}</th>
+                        <th>{e.eventId?.nombreDelEvento}</th>
+                        <th>{e.statusPago.status}</th>
+                        <th><button className="btn btn-success" onClick={() => toEvent(e.eventId._id)}>detalle evento</button></th></> : null )  }
+                    
+        
                 </tr>
             ))}
 
             </>           
-       
+
     )
 }
